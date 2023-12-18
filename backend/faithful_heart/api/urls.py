a421e46ad7c9340bcc021c6ad129ab148500b66f
Кадыров Руслан <kadyrovrr88@mail.ru>
2023-12-18 22:34:29 +0500

    Исправлены замечания ревью

--- backend/faithful_heart/api/urls.py
+++ backend/faithful_heart/api/urls.py
@@ -1,12 +1,17 @@
-from django.urls import include, path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.urls import path
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 
 from .views import APILogoutView
 
 app_name = 'api'
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path(
+        'obtain_token/',
+        TokenObtainPairView.as_view(),
+        name='token_obtain_pair'
+    ),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('logout_token/', APILogoutView.as_view(), name='logout_token'),
 ]
--- backend/faithful_heart/api/views.py
+++ backend/faithful_heart/api/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework_simplejwt.token_blacklist.models import OutstandingToken, BlacklistedToken
+from rest_framework_simplejwt.token_blacklist.models import (BlacklistedToken,
+                                                             OutstandingToken)
 from rest_framework_simplejwt.tokens import RefreshToken
 
 

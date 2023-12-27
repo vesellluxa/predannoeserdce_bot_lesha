9311aa298f6bf6aac7c8c880ded674fb1c54aadb
Алексей Шевич <lehashevich@gmail.com>
2023-12-27 21:31:35 +0300

    refractoring

--- backend/faithful_heart/faithful_heart/settings.py
+++ backend/faithful_heart/faithful_heart/settings.py
@@ -63,16 +63,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "faithful_heart.wsgi.application"
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-#         'NAME': os.getenv('DB_NAME', default='postgres'),
-#         'USER': os.getenv('POSTGRES_USER', default='postgres'),
-#         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
-#         'HOST': os.getenv('DB_HOST', default='db'),
-#         'PORT': os.getenv('DB_PORT', default='5432')
-#     }
-# }
 
 if DEBUG:
     DATABASES = {

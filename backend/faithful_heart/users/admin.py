6debebb0af5ee67109db794f7e15a18baa84c01f
Arseniy Kapshtyk <kapshtykas@yandex.ru>
2023-12-30 10:54:33 +0200

    Remove chat_id from list_display in users.admin.UserAdmin

--- backend/faithful_heart/users/admin.py
+++ backend/faithful_heart/users/admin.py
@@ -5,25 +5,26 @@ from users.models import TelegramUser, User
 
 class TelegramUserAdmin(admin.ModelAdmin):
     list_display = (
-        'pk',
-        'username',
-        'email',
-        'name',
-        'second_name',
-        'surname',
-        'phone_number',
-        'chat_id'
+        "pk",
+        "username",
+        "email",
+        "name",
+        "second_name",
+        "surname",
+        "phone_number",
+        "chat_id",
     )
-    list_display_links = ('username', 'email', 'name',)
-    list_filter = ('chat_id',)
-    search_fields = ('username', 'surname', 'email',  'phone_number')
+    list_display_links = (
+        "username",
+        "email",
+        "name",
+    )
+    list_filter = ("chat_id",)
+    search_fields = ("username", "surname", "email", "phone_number")
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = (
-        'pk',
-        'chat_id'
-    )
+    list_display = ("pk",)
 
 
 admin.site.register(TelegramUser, TelegramUserAdmin)

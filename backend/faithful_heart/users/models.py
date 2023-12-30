8ddafd85b4c2f5e5c5478b6596bbeeecec856da4
Алексей Шевич <lehashevich@gmail.com>
2023-12-31 01:49:16 +0300

    my last commit goodbye friends

--- backend/faithful_heart/users/models.py
+++ backend/faithful_heart/users/models.py
@@ -7,7 +7,7 @@ from faithful_heart import constants
 
 class TimeMixin:
     created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
